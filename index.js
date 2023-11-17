4db036d7fb99a98f90162fd7961ac0b0c159edea
karlavale <karla.nw@hotmail.com>
2023-11-17 17:41:50 -0300

    desafio finalizado

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Olá Mundo")
+let nomeHeroi = "Jenos"
+let xpHeroi = 5500
+let nivel;
+
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 6001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de "+ nivel)
