9a119b17113a1057eb372267371b7e7a608cb68b
karlavale <karla.nw@hotmail.com>
2023-12-20 17:59:23 -0300

    commit initial

---
+++ calculadoraRankeada.js
@@ -0,0 +1,26 @@
+function calcularNivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    const mensagem = `O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel}`;
+    return mensagem;
+}
+
+const resultado = calcularNivel(80, 5);
+console.log(resultado);
